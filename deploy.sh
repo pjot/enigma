cb31109b564de4ceadbc3c2dc260e9014b149a7c
Peter Bergström <petb@redpill-linpro.com>
2013-01-26 01:00:15 +0100

    Fixed script

--- deploy.sh
+++ deploy.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 # Clear Twig cache directory
-rm -r cache/*
+if [ -e cache/* ]
+    then rm -r cache/*
+fi
 # Update database
 mysql -u root -p enigma < schema.sql
 # Remove schema file
-rm schema.sql
-# Remove deploy script
-rm deploy.sh
+rm schema.sql README.md deploy.sh
