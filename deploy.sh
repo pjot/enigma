07097beaa25d92967a79ee82eda9c2d7cb5cc4b8
Peter Bergstrom <pjot.gm@gmail.com>
2013-02-07 11:34:38 +0100

    Fixed deploy script

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 # Update database
+echo "Database password"
 mysql -u root -p enigma < levels.sql
 # Remove files
-rm schema.sql README.md deploy.sh generate_schema.sql
+rm levels.sql README.md deploy.sh generate_schema.sh
