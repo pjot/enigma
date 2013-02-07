2a8127f8971ff596aa05971ff11c60b72f4838d2
Peter Bergstrom <pjot.gm@gmail.com>
2013-02-07 11:30:28 +0100

    Cache folder is no longer used.

--- deploy.sh
+++ deploy.sh
@@ -1,10 +1,6 @@
 #!/bin/bash
 
-# Clear Twig cache directory
-if [ -e cache/* ]
-    then rm -r cache/*
-fi
 # Update database
 mysql -u root -p enigma < levels.sql
-# Remove schema file
+# Remove files
 rm schema.sql README.md deploy.sh generate_schema.sql
